7489943c15e8876e1dc5ba6e8f21f5d43eb13d51
Wiels <edward.wiels@student.kuleuven.be>
2021-11-29 16:31:15 +0100

    working mouse

--- server.js
+++ server.js
@@ -1,1 +1,13 @@
 console.log("Up and running");
+//Load library in variable
+let express = require("express");
+//result of running the library?
+let app = express();
+
+let port = 3000;
+
+let server = app.listen(port)
+
+console.log("Server is running on http://localhost:"+port)
+//Where to get server files from?
+app.use(express.static("public"))
